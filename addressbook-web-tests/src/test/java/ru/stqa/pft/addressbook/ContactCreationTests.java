22f8b6dfed9743b5dfc80ead6479cbc9462fec1f
Shinkai <alinlaegnor@gmail.com>
2021-04-18 03:54:40 +0300

    Создан общий базовый класс для тестов TestBase с общими для всех тестов методами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -16,14 +16,14 @@ public class ContactCreationTests {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost:8080/addressbook/");
-    login(new LoginData("admin", "secret"));
+    login("admin", "secret");
   }
 
-  private void login(LoginData loginData) {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(loginData.getUsername());
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(loginData.getPassword());
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
