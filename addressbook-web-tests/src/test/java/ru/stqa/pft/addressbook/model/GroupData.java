d64ae1bb9fccea9a9c798997b10248949ab25e48
Shinkai <alinlaegnor@gmail.com>
2021-05-03 00:32:41 +0300

    Проверка создания нужной группы (поиск max id в коллекции)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,25 +1,43 @@
 package ru.stqa.pft.addressbook.model;
 
-import java.util.Objects;
-
 public class GroupData {
-  private final String id;
+  private int id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getHeader() {
+    return header;
+  }
+
+  public String getFooter() {
+    return footer;
   }
 
   @Override
@@ -37,30 +55,14 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (!Objects.equals(id, groupData.id)) return false;
-    return Objects.equals(name, groupData.name);
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
+    int result = id;
     result = 31 * result + (name != null ? name.hashCode() : 0);
     return result;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getHeader() {
-    return header;
-  }
-
-  public String getFooter() {
-    return footer;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -12,8 +13,19 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.navigate().gotoGroupPage();
     List<GroupData> before = app.group().getGroupList();
-    app.group().createGroup(new GroupData("test1", null, null));
+    GroupData group = new GroupData("test1", null, null);
+    app.group().createGroup(group);
     List<GroupData> after = app.group().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    int max = 0;
+    for (GroupData g: after) {
+      if (g.getId() > max) {
+        max = g.getId();
+      }
+    }
+    group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 }
