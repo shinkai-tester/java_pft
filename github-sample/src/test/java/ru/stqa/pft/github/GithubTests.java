c78dd2d2ff6ecbb6e54cb1d7945dc71283c0c114
Shinkai <alinlaegnor@gmail.com>
2021-06-07 18:14:53 +0300

    REST: get commits from GitHub

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableBiMap;
+import com.jcabi.github.Coordinates;
+import com.jcabi.github.RepoCommit;
+import com.jcabi.github.RepoCommits;
+import com.jcabi.github.RtGithub;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    RtGithub github = new RtGithub("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");
+    RepoCommits commits = github.repos().get(
+            new Coordinates.Simple("shinkai-tester", "java_pft")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableBiMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
