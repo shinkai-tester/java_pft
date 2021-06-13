5a1f50bacfdd82dadaf81ddbd24048ca3b18bbb5
Shinkai <alinlaegnor@gmail.com>
2021-06-13 05:45:20 +0300

    The possibility to start the browser remotely is added to addressbook

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,14 +1,18 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -31,27 +35,50 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties = new Properties();
     properties.load(new FileReader(new File(String.format("src/test/resources/config/%s.properties", target))));
+    String baseUrl = properties.getProperty("web.baseUrl");
+    String seleniumGrid = properties.getProperty("selenium.server");
+    String adminLogin = properties.getProperty("web.adminLogin");
+    String adminPassword = properties.getProperty("web.adminPassword");
+    String geckoDriverPath = properties.getProperty("web.geckoDriverPath");
+    String chromeDriverPath = properties.getProperty("web.chromeDriverPath");
+    String ieDriverPath = properties.getProperty("web.ieDriverPath");
 
     dbHelper = new DbHelper();
 
-    switch (browser) {
-      case BrowserType.CHROME:
-        wd = new ChromeDriver();
-        break;
-      case BrowserType.FIREFOX:
-        wd = new FirefoxDriver();
-        break;
-      case BrowserType.IE:
-        wd = new InternetExplorerDriver();
-        break;
+    if ("".equals(seleniumGrid)) {
+      switch (browser) {
+        case BrowserType.CHROME:
+          setDriverPath("webdriver.chrome.driver", chromeDriverPath);
+          wd = new ChromeDriver();
+          break;
+        case BrowserType.FIREFOX:
+          setDriverPath("webdriver.gecko.driver", geckoDriverPath);
+          wd = new FirefoxDriver();
+          break;
+        case BrowserType.IE:
+          setDriverPath("webdriver.ie.driver", ieDriverPath);
+          wd = new InternetExplorerDriver();
+          break;
+        default:
+          throw new IOException("Unrecognized browser: " + browser);
+      }
+    } else {
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(seleniumGrid), capabilities);
     }
+
     wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
+    wd.get(baseUrl);
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
+    sessionHelper.login(adminLogin, adminPassword);
+  }
+
+  private void setDriverPath(String driver, String path) {
+    System.setProperty(String.format("webdriver.%s.driver", driver), path);
   }
 
   public void stop() {
